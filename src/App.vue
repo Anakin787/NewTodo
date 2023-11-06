295a58f24c70dad185e77feee41d21325cb1bfe6
Anakin787 <seonbi1998@naver.com>
2023-11-06 14:58:48 +0900

    Test Commit

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/about">Commit Test</router-link>
   </nav>
-  <router-view/>
 </template>
 
 <style>
