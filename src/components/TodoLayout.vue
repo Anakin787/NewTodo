c6bfb3157d230072b3b1ea3cee19f746dbeb5e72
Anakin787 <seonbi1998@naver.com>
2024-02-16 19:00:28 +0900

    2월16일 작업

--- src/components/TodoLayout.vue
+++ src/components/TodoLayout.vue
@@ -1,18 +1,25 @@
 <template>
-  <v-container class="text-center font-weight-bold text-h6 outlined">
+  <v-container class="text-center font-weight-bold text-h6 outlined ">
     <v-card class="elevation-3 w-50 text-center mx-auto" style="font-family: yeonsung; background: linear-gradient(45deg, #ff9a9e 0%, #fad0c4 99%, #fad0c4 100%)">
       <v-card-title class="font-weight-black"/>갓생일지
       <v-row>
         <v-col cols="9" align="right">
           <v-form
-            v-on:submit="submit"
+            v-on:submit.prevent="submit()"
             >
             <v-text-field v-model="inputTodo" class="w-75 ml-3" color="white" :rules="charCount" clearable label="할 일을 입력해 주세요."></v-text-field>
           </v-form>
         </v-col >
         <v-col cols="3" class="mt-3" align="left">
-            <v-icon class="add-icon-hover" type="sumbit" @click="test()">mdi-plus</v-icon>
+            <v-icon class="add-icon-hover" type="sumbit" @click="submit()">mdi-plus</v-icon>
         </v-col>
+      </v-row>
+
+      <v-row v-for="(item,i) in todoList" :key="i">
+        <v-col cols="2">▶️</v-col>
+        <v-col>{{ item }}</v-col>
+        <v-col>완료버튼</v-col>
+        <v-col>삭제</v-col>
       </v-row>
     </v-card>
   </v-container>
@@ -27,16 +34,21 @@ export default {
   data(){
     return{
       inputTodo : "",
+      todoList : [],
       charCount: [v => !(v && v.length >= 50) || '50자 이상 입력할 수 없습니다.']
     }
   },
+  mounted(){
+    let getTodo = localStorage.getItem("todolist") || []
+    this.todoList = JSON.parse(getTodo) 
+  },
   methods: {
-    test(){
-      console.log("test")
-    },
     submit(){
-      // submit해서 실행되는 함수
-      console.log("test2")
+      if(this.inputTodo !== ""){
+        this.todoList.push(this.inputTodo)
+        localStorage.setItem("todolist", JSON.stringify(this.todoList))
+        this.inputTodo = ''
+      }
     }
   }
 }
