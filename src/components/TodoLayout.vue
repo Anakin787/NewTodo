eee705875f826da5baef4c35dab0f9f3042c1a1a
jujeong <jujeong@ncurity.com>
2025-07-21 22:38:24 +0900

    TodoLayout UI변경 및 등록된 할일 클릭시 상세정보창 개발 & 할일 Session Storage에 저장

--- src/components/TodoLayout.vue
+++ src/components/TodoLayout.vue
@@ -1,15 +1,14 @@
 <template>
-  <v-container class="text-center font-weight-bold text-h6 outlined">
-    <v-card id="card" class="elevation-3 text-center mx-auto width: 100%;">
-      <div class="font-weight-black mt-2 mb-1" style="font-size: 24px;">갓생일지</div>
+  <v-container fluid class="text-center font-weight-bold text-h6 outlined pa-0">
+    <v-card id="card" class="elevation-3 text-center">
       <v-row>
-        <v-col cols="9">
-          <v-form ref="form" @submit.prevent='inputData'>
+        <v-col cols="11">
+          <v-form ref="form" @submit.prevent='inputData' class="mt-5">
             <v-text-field v-model="inputTodo" class="ml-5" variant="outlined" :rules="inputRules" clearable
               label="할 일을 입력해 주세요."></v-text-field>
           </v-form>
         </v-col>
-        <v-col cols="3" class="pl-0">
+        <v-col cols="1" class="pl-0 mt-5">
           <v-btn class="add-icon-hover" variant="outlined" color="warning" style="height: 56px; width: 100px;">
             <v-icon type="sumbit" @click="submitIfValid()"> mdi-plus </v-icon>
           </v-btn>
@@ -18,39 +17,75 @@
       <v-divider class="mb-5" thickness="5" style="color: black;" />
 
       <!-- 새로고침시 내용이 남아있어야함 -->
-      <v-row v-for="(list, idx) in todoList" :key="idx">
-        <v-col cols="2">▶</v-col>
-        <!-- 글씨를 눌렀을때 완료표시가 되어야함 -->
-        <v-col align="left"> {{ list }}</v-col>
-        <!-- icon클릭시 해당 인덱스의 값이 삭제되어야 함 -->
-        <v-col cols="2"><v-icon> mdi-delete-outline </v-icon></v-col>
+      <v-row justify="center" style="width: 100%;" class="mx-auto">
+        <v-col
+          v-for="(todo, idx) in todoList"
+          :key="idx"
+          cols="12"
+          class="d-flex"
+        > 
+          <v-card
+            class="pa-3 flex-grow-1 d-flex align-center todo-card"
+            variant="outlined"
+            @click="onCardClick(idx)"
+          >
+            <div style="flex: 0 0 auto;">
+              <v-checkbox v-model="todo.completed" color="primary" hide-details @click.stop></v-checkbox>
+            </div>
+            <span class="ml-3 text-left" style="flex: 1;">{{ todo.text }}</span>
+          </v-card>
+        </v-col>
       </v-row>
     </v-card>
   </v-container>
+
+  <!-- 우측 상세 레이아웃 -->
+  <v-navigation-drawer
+    v-model="showDetails"
+    fixed
+    location="right"
+    temporary
+    width="300"
+    attach="body"
+  >
+    <div class="pa-4">
+      <h3 class="text-h6 font-weight-bold mb-4">할 일 상세</h3>
+      <p v-if="selectedTodo">{{ selectedTodo.text }}</p>
+    </div>
+  </v-navigation-drawer>
 </template>
 
 <script>
 export default {
   name: 'TodoLayout',
-  props: {
-  },
-
   data() {
     return {
       inputTodo: "",
       inputRules: [
-        v => !!v || '공백은 입력할 수 없습니다.',
-        v => v.trim().length > 0 || '공백 입력은 허용되지 않습니다.',
         v => !(v && v.length >= 50) || '50자 이상 입력할 수 없습니다.'
       ],
 
-      todoList: []
+      todoList: [],
+      showDetails: false,
+      selectedTodo: null
     }
   },
-  // mounted(){
-  //   let getTodo = localStorage.getItem("todolist") || []
-  //   this.todoList = JSON.parse(getTodo) 
-  // },
+  mounted(){
+    const savedTodos = sessionStorage.getItem("todolist");
+    if (savedTodos) {
+      this.todoList = JSON.parse(savedTodos);
+    }
+  },
+  
+  watch: {
+    todoList: {
+      handler(newTodoList) {
+        sessionStorage.setItem("todolist", JSON.stringify(newTodoList));
+      },
+      deep: true
+    }
+  },
+  
   methods: {
     inputData() {
       if (this.inputTodo.trim() === '') {
@@ -58,15 +93,19 @@ export default {
         alert('입력값이 공백입니다. 올바른 값을 입력해 주세요.');
         return;
       }
-      this.todoList.push(this.inputTodo)
+      this.todoList.push({ text: this.inputTodo, completed: false })
       this.inputTodo = ''
-      // console.log("inputData",this.todoList[0])
     },
 
     submitIfValid() {
       if (this.$refs.form.validate()) {
         this.inputData();
       }
+    },
+
+    onCardClick(index) {
+      this.selectedTodo = this.todoList[index];
+      this.showDetails = true;
     }
   }
 }
@@ -80,7 +119,7 @@ export default {
 .add-icon-hover:hover .v-icon {
   transition: all 1s;
   transform: rotate(90deg);
-  color: white;
+  color: black;
 }
 
 .v-input__details {
@@ -90,7 +129,11 @@ export default {
 #card {
   background: wheat;
   font-family: yeonsung;
-  width: 35%;
-  height: 800px;
+  min-height: 100vh;
+}
+
+/* pointer cursor for card */
+.todo-card {
+  cursor: pointer;
 }
 </style>
